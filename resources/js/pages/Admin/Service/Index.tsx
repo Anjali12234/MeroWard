01e7a98dc92e2bb6ba721f0b03b32fb9d2c72e0b
Anjali12234 <anjisharma649@gmail.com>
2026-09-25 09:06:38 +0545

    service index page correct

--- resources/js/pages/Admin/Service/Index.tsx
+++ resources/js/pages/Admin/Service/Index.tsx
@@ -34,7 +34,7 @@ export default function ServiceIndex({ service }: Props) {
                     <Button asChild>
                         <Link href={create().url} className="flex items-center gap-2">
                             <Plus className="h-4 w-4" />
-                            Create Employee
+                            Create Service
                         </Link>
                     </Button>
                 </div>
@@ -56,7 +56,7 @@ ServiceIndex.layout = (page: React.ReactNode) => (
     <AppLayout
         breadcrumbs={[
             {
-                title: 'Employee',
+                title: 'Service',
                 href: index().url,
             },
         ]}
